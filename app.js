ff451873707a38bc5a68b263c488e291d93fa19e
LinaKrdozo <linakrdozo13@gmail.com>
2024-02-23 11:09:49 -0500

    modificaciones rutas

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) => res.sendFile(path.resolve("./views/index.html")));
 app.get("/login", (req, res) => res.sendFile(path.resolve("./views/login.html")));
 app.get("/carrito", (req, res) => res.sendFile(path.resolve("./views/productCart.html")));
-app.get("/detalle", (req, res) => res.sendFile(path.resolve("./views/productDetail.html")));
+app.get("/productos", (req, res) => res.sendFile(path.resolve("./views/productDetail.html")));
 app.get("/registro", (req, res) => res.sendFile(path.resolve("./views/register.html")));
 
 app.get("*", (req, res) => res.status(404).send("404 not found. <br> ¡Houston, tenemos un problema!"));
