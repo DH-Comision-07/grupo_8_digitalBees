d05d3570dfc307be47822c3459ba88c623bb3c76
LinaKrdozo <linakrdozo13@gmail.com>
2024-02-25 10:57:08 -0500

    agregando vista emergente producto

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get("/", (req, res) => res.sendFile(path.resolve("./views/index.html")));
 app.get("/login", (req, res) => res.sendFile(path.resolve("./views/login.html")));
 app.get("/carrito", (req, res) => res.sendFile(path.resolve("./views/productCart.html")));
 app.get("/productos", (req, res) => res.sendFile(path.resolve("./views/productDetail.html")));
+app.get("/producto", (req, res) => res.sendFile(path.resolve("./views/product.html")));
 app.get("/registro", (req, res) => res.sendFile(path.resolve("./views/register.html")));
 
 app.get("*", (req, res) => res.status(404).send("404 not found. <br> ¡Houston, tenemos un problema!"));
