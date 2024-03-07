22a784b7deaf5d5b6dccc68572b62b7d09943e01
LinaKrdozo <linakrdozo13@gmail.com>
2024-03-06 21:00:43 -0500

    distribución de archivos en products y users, modificación de rutas en app.js

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const listaDeProductos= importarArchivoProductos()
 const port = 3030;
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views" );
+app.set("views", __dirname + "/views/" );
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get("/", (req, res) => res.render("index", {listaDeProductos}));
-app.get("/productos", (req, res) => res.render("listaProductos", {listaDeProductos}));
-app.get("/login", (req, res) => res.render("login"));
-app.get("/registro", (req, res) => res.render("register"));
-app.get("/detalle", (req, res) => res.render("product-detail"));
-app.get("/carrito", (req, res) => res.render("productCart", {listaDeProductos}));
+app.get("/productos", (req, res) => res.render("products/listaProductos", {listaDeProductos}));
+app.get("/login", (req, res) => res.render("users/login"));
+app.get("/registro", (req, res) => res.render("users/register"));
+app.get("/detalle", (req, res) => res.render("products/product-detail"));
+app.get("/carrito", (req, res) => res.render("products/productCart", {listaDeProductos}));
 
 app.get("*", (req, res) => res.status(404).send("404 not found. <br> ¡Houston, tenemos un problema!"));
 
