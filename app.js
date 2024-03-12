c7abcabfd64066ee64dc6eb346ceb624911d138a
Lina Cardozo <linakrdozo13@gmail.com>
2024-03-11 23:05:00 -0500

    se agrego carperta src, controllers y  routers  de producto y ruteos en app.js

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require("express");
-
 const app = express();
-
 const path = require("path");
+const indexRouter=require('./src/routes/index.routes')
+const productsRouter=require('./src/routes/products.routes')
+const shopingCarRouter=require('./src/routes/shopingCar.routes')
+const productDetailRouter=require('./src/routes/products.routes')
 
-const importarArchivoProductos = require("./views/database/datosProductos");
-const listaDeProductos= importarArchivoProductos()
+//const importarArchivoProductos = require("./src/database/datosProductos");
+//const listaDeProductos= importarArchivoProductos()
 
 const port = 3030;
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views/" );
-app.use(express.static(path.join(__dirname + '/public')));
+app.set("views", path.join(__dirname,'src/views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => res.render("index", {listaDeProductos}));
-app.get("/productos", (req, res) => res.render("products/listaProductos", {listaDeProductos}));
+//app.get("/", (req, res) => res.render("index", {listaDeProductos}));
+app.use('/', indexRouter);
+//app.get("/productos", (req, res) => res.render("products/listaProductos", {listaDeProductos}));
+app.use('/productos', productsRouter);
+//app.get("/carrito", (req, res) => res.render("products/productCart", {listaDeProductos}));
+app.use('/carrito', shopingCarRouter);
+//app.get("/detalle", (req, res) => res.render("products/product-detail"));
+app.use('/detalle', productDetailRouter);
+
+
 app.get("/login", (req, res) => res.render("users/login"));
 app.get("/registro", (req, res) => res.render("users/register"));
-app.get("/detalle", (req, res) => res.render("products/product-detail"));
-app.get("/carrito", (req, res) => res.render("products/productCart", {listaDeProductos}));
+
+
 
 app.get("*", (req, res) => res.status(404).send("404 not found. <br> ¡Houston, tenemos un problema!"));
 
---
+++ src/controllers/productsController.js
@@ -0,0 +1,21 @@
+let productos = require('../views/database/datosProductos');
+const listaDeProductos= productos()
+let producto;
+const path = require('path');
+
+const productsController={
+    // esto es un comentario
+    getAll:(req, res) =>res.render("products/listaProductos", {"listaDeProductos" : listaDeProductos}),
+    getMain:(req, res) =>res.render("products/productCart", {listaDeProductos}),
+    getOne: (req, res) => {
+        let idProducto=req.params.id;
+            productoFiltrado = listaDeProductos.filter(function(producto){
+                return producto.id == idProducto
+            })
+        
+        res.render('products/product-detail', {"producto": productoFiltrado[0]} )
+    }
+    
+}
+
+module.exports = productsController;
