531c2f3194ba66f8602659892512d32d33c08b3a
Lina Cardozo <linakrdozo13@gmail.com>
2024-05-17 08:10:26 -0500

    agregando slider

---
+++ public/js/slider.js
@@ -0,0 +1,384 @@
+/*SLIDER CONFIGURATION*/
+const slides = [
+	{
+		url:
+			"https://images.unsplash.com/photo-1627587780063-1655815c958b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzNTIzNzA2MA&ixlib=rb-1.2.1&q=85",
+		heading: "Great Food",
+		tagline: "Enjoy our famed selection of World Class Meals"
+	},
+	{
+		url:
+			"https://images.unsplash.com/photo-1633119368937-9993a58cdaaa?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzNTIzNzA2MA&ixlib=rb-1.2.1&q=85",
+		heading: "The Best Drinks",
+		tagline: "Cool off with our exotic drinks"
+	},
+	{
+		url:
+			"https://images.unsplash.com/photo-1634370058578-41e7092f261f?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzNTIzNzA2MA&ixlib=rb-1.2.1&q=85",
+		heading: "Plenty of Adventure",
+		tagline: "Go on hikes in the beautiful landscapes overlooking the resort"
+	},
+	{
+		url:
+			"https://images.unsplash.com/photo-1634048766558-916c82e0ba91?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzNTIzNzA2MA&ixlib=rb-1.2.1&q=85",
+		heading: "Vibrant Nightlife",
+		tagline:
+			"It's a ten minute drive to the city, with a variety of activities for a fun night out on the town"
+	},
+	{
+		url:
+			"https://images.unsplash.com/photo-1633596667725-e3f715436023?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzNTIzNzA2MA&ixlib=rb-1.2.1&q=85",
+		heading: "Get in the Zone",
+		tagline:
+			"Whether it's work, study, or a retreat, our serene environment will provide all the zen you need"
+	},
+	{
+		url:
+			"https://images.unsplash.com/photo-1633885278462-f6e52e3081fb?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzNTIzNzA2MA&ixlib=rb-1.2.1&q=85",
+		heading: "Recharge your batteries",
+		tagline:
+			"When you need a getaway from all the hustle, or just need to enjoy nature, we'll be right here"
+	}
+];
+
+const options = {
+	heading_font: "Roboto Slab",
+	tagline_font: "Raleway",
+	heading_color: "#00a34c",
+	tagline_color: "#fff",
+	arrow_color: "#627231",
+	dot_color: "#627231",
+	hover_color: "#d2e286",
+	active_color: "#00a34c",
+	filters: "brightness(30%)",
+	heading_size: "3rem",
+	tagline_size: "1.25rem"
+};
+/*SLIDER LOGIC*/
+let activeSlide;
+const carousel = document.getElementById("slide-container");
+let currScript = document.currentScript;
+
+window.onload = () => {
+	renderSlides();
+	activeSlide = 0;
+	updateActiveLink();
+	document.getElementById("prevSlide").addEventListener("click", prevSlide);
+	document.getElementById("nextSlide").addEventListener("click", nextSlide);
+
+	let autoplaySlider = setInterval(nextSlide, 4000);
+	document.getElementById("prevSlide").addEventListener("click", () => {
+		clearInterval(autoplaySlider);
+	});
+
+	document.getElementById("nextSlide").addEventListener("click", () => {
+		clearInterval(autoplaySlider);
+		autoplaySlider = setInterval(nextSlide, 4000);
+	});
+};
+
+function createSlide(item, index) {
+	const newSlide = document.createElement("div");
+	newSlide.className = "slide";
+	newSlide.id = "slide" + parseInt(index); /*Critical: Modify with caution*/
+	let image = document.createElement("img");
+	image.src = item.url;
+	image.alt = "carousel slide " + index + " about " + item.tagline;
+	newSlide.appendChild(image);
+	let slideText = document.createElement("div");
+	slideText.className = "slideText";
+	let heading = document.createElement("h3");
+	heading.className = "slideHeading";
+	heading.innerText = item.heading;
+	slideText.appendChild(heading);
+	let tagline = document.createElement("p");
+	tagline.className = "slideTagline";
+	tagline.innerText = item.tagline;
+	slideText.appendChild(tagline);
+	newSlide.appendChild(slideText);
+	return newSlide;
+}
+
+function createSlideLink(index) {
+	const newLink = document.createElement("a");
+	newLink.id = "slideBtn" + parseInt(index); /*Critical: Modify with Caution*/
+	let listItem = document.createElement("li");
+	listItem.className = "slideBtn";
+	newLink.appendChild(listItem);
+	newLink.addEventListener("click", async function (event) {
+		event.preventDefault();
+		const nextSlide = document.getElementById("slide" + index);
+		carousel.scroll({
+			top: nextSlide.offsetTop,
+			left: nextSlide.offsetLeft,
+			behavior: "smooth"
+		});
+		if (event.target.id !== "slideBtn" + activeSlide) {
+			await getActiveSlide();
+		}
+	});
+	return newLink;
+}
+
+function createStyles() {
+	const styles = document.createElement("style");
+	styles.id = "styleOptions";
+	styles.innerHTML = `
+	.slide img {
+		filter: ${options.filters};
+	}
+	.slideHeading {
+		font-family: ${options.heading_font};
+		font-size: ${options.heading_size};
+		color: ${options.heading_color};
+	}
+	.slideTagline {
+		font-family: ${options.tagline_font};
+		font-size: ${options.tagline_size};
+		color: ${options.tagline_color};
+	}
+	.slideNav svg {
+		fill: ${options.arrow_color};
+	}
+	.slideBtn {
+		background-color: ${options.dot_color};
+	}
+	.pagination a.active > li.slideBtn {
+		background-color: ${options.active_color};
+	}
+	.pagination a:hover > li.slideBtn {
+		background-color: ${options.hover_color};
+	}
+	.prev-slide:hover svg,
+	.next-slide:hover svg {
+		fill: ${options.hover_color};
+	}
+	`;
+	carousel.appendChild(styles);
+}
+
+async function renderSlides() {
+	const container = document.querySelector("#slide-container");
+	container.innerHTML = "";
+	const pagination = document.querySelector("#pagination");
+	pagination.innerHTML = "";
+	slides.map((item, index) => {
+		let slide = createSlide(item, index);
+		container.appendChild(slide);
+		let link = createSlideLink(index);
+		pagination.appendChild(link);
+	});
+	createStyles();
+	getActiveSlide();
+}
+
+function getActiveSlide() {
+	const allSlides = document.querySelectorAll(".slide");
+
+	const observer = new IntersectionObserver(
+		(entries, observer) => {
+			entries.forEach((entry) => {
+				if (entry.intersectionRatio > 0.5) {
+					activeSlide = parseInt(entry.target.id.substr(5));
+					updateActiveLink();
+				}
+			});
+		},
+		{
+			root: document.querySelector("#slide-container"),
+			rootMargin: "0px",
+			threshold: 1.0
+		}
+	);
+
+	allSlides.forEach((slide) => {
+		observer.observe(slide);
+	});
+}
+
+async function updateActiveLink() {
+	let btns = document.querySelectorAll("a[id^='slideBtn']");
+	btns.forEach((btn) => {
+		btn.classList.toggle("active", "slideBtn" + activeSlide === btn.id);
+	});
+}
+
+async function prevSlide(evt) {
+	evt.preventDefault();
+	await getActiveSlide();
+
+	let incomingSlideBtn;
+	if (activeSlide > 0) {
+		incomingSlideBtn = "slideBtn" + (activeSlide - 1);
+	} else {
+		incomingSlideBtn = "slideBtn" + (slides.length - 1);
+	}
+
+	await document.getElementById(incomingSlideBtn).click();
+}
+
+async function nextSlide(evt) {
+	evt?.preventDefault();
+	await getActiveSlide();
+
+	let incomingSlideBtn;
+	if (activeSlide < slides.length - 1) {
+		incomingSlideBtn = "slideBtn" + (activeSlide + 1);
+	} else {
+		incomingSlideBtn = "slideBtn0";
+	}
+
+	await document.getElementById(incomingSlideBtn).click();
+}
+/*END OF SLIDER*/
+
+/*CUSTOMIZING OPTIONS: Add Slides, Delete Slides and Settings
+  SPACING/SIZING OPTIONS NOT INCLUDED: You may edit Margins/Padding/Line-Height/Letter-Spacing/Font-Size in the CSS rulesets
+  Maybe fork it and add spacing/sizing options?*/
+const addSlide = (evt) => {
+	evt.preventDefault();
+	evt.stopPropagation();
+	const newSlide = {
+		url: document.querySelector("input[name='url']").value,
+		heading: document.querySelector("input[name='heading']").value,
+		tagline: document.querySelector("input[name='tagline']").value
+	};
+	if (!newSlide.url) {
+		alert("Ërror: Provide a link for an image");
+		return null;
+	}
+	if (
+		slides.some(
+			(slide) =>
+				slide.heading === newSlide.heading &&
+				slide.tagline === newSlide.tagline &&
+				slide.url === newSlide.url
+		)
+	) {
+		alert("Error: This exact slide already exists");
+		return null;
+	}
+	slides.push(newSlide);
+	renderSlides();
+};
+
+const delSlide = (evt) => {
+	evt.preventDefault();
+	evt.stopPropagation();
+	let slideIndex = document
+		.querySelector("input[name='slideToDelete']")
+		.value.split(",")
+		.filter((x) => x !== "")
+		.map((x) => {
+			let y = parseInt(x.trim());
+			if (!isNaN(y)) return y;
+		});
+	if (!slideIndex?.length) {
+		alert("Ërror: Provide at least 1 slide number to delete");
+		return null;
+	}
+	const slidesToDel = [];
+	slideIndex.map((ordinalNumber) => {
+		let index = ordinalNumber - 1;
+		if (slides[index]) {
+			slidesToDel.push(slides[index]);
+		} else {
+			alert("Error: Slide " + ordinalNumber + " does not exist");
+			return null;
+		}
+	});
+	slidesToDel.map((forTrash) => {
+		let delIndex = slides.indexOf(forTrash);
+		if (delIndex !== -1) {
+			slides.splice(delIndex, 1);
+		}
+	});
+	renderSlides();
+};
+
+const setOptions = (evt) => {
+	evt.preventDefault();
+	evt.stopPropagation();
+	[...evt.target.elements].forEach((optionField) => {
+		if (optionField.value) {
+			options[optionField.name] = optionField.value;
+			if (["heading_font", "tagline_font"].includes(optionField.name)) {
+				alert("Remember to add imports to the html/css for fonts");
+			}
+		}
+	});
+	renderSlides();
+};
+
+function copyToClipboard(event) {
+	const output = document.querySelector("textarea[name='codebox']");
+	output.select();
+	output.setSelectionRange(0, 99999);
+	navigator.clipboard.writeText(output.value);
+	event.target.innerText = "Copied To Clipboard!";
+	event.target.style.backgroundColor = "#00a34c";
+	setTimeout(() => {
+		event.target.innerText = "Copy To Clipboard";
+		event.target.style.backgroundColor = "#627231";
+	}, 600);
+}
+
+function copyObjects() {
+	document.querySelector(
+		"#codebox"
+	).innerText = `const slides = ${JSON.stringify(slides)} \n\n
+  const options = ${JSON.stringify(options)}`;
+	document.querySelector(".overlay").style.display = "block";
+}
+
+function copyHTML() {
+	carousel.removeChild(carousel.querySelector("#styleOptions"));
+	let markup = document.querySelector(".slider").innerHTML;
+	document.querySelector(
+		"#codebox"
+	).innerText = `<div class="slider">\n${markup}\n</div>`;
+	document.querySelector(".overlay").style.display = "block";
+}
+
+function copyCSS() {
+	let finalCss = "";
+	const stylesheets = document.querySelectorAll("style");
+	[...stylesheets].map((sheet) => {
+		if (sheet.innerHTML.includes("/* START OF SLIDER STYLES */")) {
+			finalCss += sheet.innerHTML.split("/* END OF SLIDER STYLES */")[0];
+		} else {
+			finalCss += sheet.innerHTML;
+		}
+	});
+	document.querySelector("#codebox").innerText = finalCss;
+	document.querySelector(".overlay").style.display = "block";
+}
+
+async function copyJS() {
+	let finalJS = "";
+	document.querySelector(".overlay").style.display = "block";
+	document.querySelector("#codebox").innerText = "Loading...";
+	await fetch(currScript.src)
+		.then(function (response) {
+			if (!response.ok) {
+				throw new Error(`Failed to get script contents: ${response.status}`);
+			}
+			return response.text();
+		})
+		.then(function (response) {
+			finalJS = response;
+			finalJS = finalJS.split("/*SLIDER LOGIC*/")[1];
+			finalJS = finalJS.split("/*END OF SLIDER*/")[0];
+			finalJS = `
+	const slides = ${JSON.stringify(slides)} \n\n 
+	const options = ${JSON.stringify(options)} \n\n\n
+	${finalJS}`;
+			document.querySelector("#codebox").innerText = finalJS;
+		}).catch((e) => {
+		alert("Can't get the contents of the script. Please, try re-running with the run command/button");
+	});
+}
+
+document.getElementById("slidecreator").addEventListener("submit", addSlide);
+document.getElementById("slidedeleter").addEventListener("submit", delSlide);
+document.getElementById("settings").addEventListener("submit", setOptions);
